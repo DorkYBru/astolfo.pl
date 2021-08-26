24dfa2d890cc80ac2c1350db5b19914e8546a8cf
DorkYBru <76796721+DorkYBru@users.noreply.github.com>
2021-08-26 12:11:05 +0200

    added jp

---
+++ assets/scripts/jp.js
@@ -0,0 +1,8 @@
+function jp2() {
+                      var doc = prompt("Gimmie da secret code",
+                          "JPlubiDzieci");
+                      if (doc != null) {
+                          document.getElementById("g").innerHTML =
+                              "Welcome to " + doc;
+                      }
+                  }
